e31e0c960b10fb62efc1bd80f333442430921618
Rohit555-ui <rohitkumar55552@gmail.com>
2019-09-23 22:40:02 +0530

    change in log file

--- logfile.py
+++ logfile.py
@@ -1,3 +1,17 @@
 import logging
 
-logging.basicConfig(filename='load.log', level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s')
+logging.basicConfig(filename='logTesting.log', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
+
+
+def sum(a, b):
+    logging.debug(a+b)
+    return a+b
+
+v1 = 10
+v2 = 20
+value_msg = "value1:= {} and value2:= {} before sum".format(v1, v2)
+logging.debug(value_msg)
+a = sum(v1, v2)
+logging.error(a)
+
+print(a)
